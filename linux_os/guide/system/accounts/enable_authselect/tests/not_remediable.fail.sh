7af99d5e1fe1b8211893183aa6ae5dbbb62f817a
BordenCastle Admin <49888529+bordencastle@users.noreply.github.com>
2025-12-21 15:32:12 -0500

    Update not_remediable.fail.sh
    Remove duplicate platform.

--- linux_os/guide/system/accounts/enable_authselect/tests/not_remediable.fail.sh
+++ linux_os/guide/system/accounts/enable_authselect/tests/not_remediable.fail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# platform = multi_platform_fedora,Oracle Linux 8,Oracle Linux 9,Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9,multi_platform_almalinux,multi_platform_al,multi_platform_al
+# platform = multi_platform_fedora,Oracle Linux 8,Oracle Linux 9,Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9,multi_platform_almalinux,multi_platform_al
 # packages = authselect,pam
 # remediation = none
 
