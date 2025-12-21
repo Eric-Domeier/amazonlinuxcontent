36ee98d3c49301dd4e196e0749d6e82702f008d2
BordenCastle Admin <49888529+bordencastle@users.noreply.github.com>
2025-12-21 15:37:11 -0500

    Update correct_max_pas_age.pass.sh
    Never had a platform, should default to all.

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/correct_max_pas_age.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/correct_max_pas_age.pass.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# platform = Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_sle,multi_platform_slmicro,multi_platform_ubuntu,multi_platform_almalinux,multi_platform_debian,multi_platform_al
 # variables = var_accounts_minimum_age_login_defs=1,var_accounts_maximum_age_login_defs=60
 # make existing entries pass
 for acct in $(awk -F: '(/^[^:]+:[^!*]/ && ($5 > var || $5 == "")) {print $1}' /etc/shadow ); do
